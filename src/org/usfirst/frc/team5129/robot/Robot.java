388d63ee28c0b12b9ca1bc6742f1c69316dc9304
Kyle Guarco <kyleguarco55@gmail.com>
2017-11-24 08:01:44 -0500

    Removed deprecated methods

--- src/org/usfirst/frc/team5129/robot/Robot.java
+++ src/org/usfirst/frc/team5129/robot/Robot.java
@@ -2,6 +2,7 @@ package org.usfirst.frc.team5129.robot;
 
 import org.usfirst.frc.team5129.subsystem.Camera;
 import org.usfirst.frc.team5129.subsystem.Drive;
+import org.usfirst.frc.team5129.subsystem.Lift;
 import org.usfirst.frc.team5129.subsystem.meta.AutoSubsystem;
 import org.usfirst.frc.team5129.subsystem.meta.Routine;
 import org.usfirst.frc.team5129.subsystem.meta.Subsystem;
@@ -9,52 +10,54 @@ import org.usfirst.frc.team5129.subsystem.meta.Subsystem;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.networktables.NetworkTable;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 
 public class Robot extends IterativeRobot {
 
-	private SendableChooser<String> choice;
+	private SendableChooser<Integer> choice;
 
 	private RobotDrive drive;
 	@SuppressWarnings("unused")
 	private Joystick stick;
 	private XboxController controller;
 
+	private Spark lift;
+
 	private Subsystem[] subs;
 	private AutoSubsystem[] auto;
 	private Routine routine;
 
-	@SuppressWarnings("deprecation")
 	@Override
 	public void robotInit() {
 		choice = new SendableChooser<>();
 		choice.initTable(NetworkTable.getTable("Yui"));
-		choice.getTable().putInt("autonomous_routine", 0);
+		choice.getTable().putString("autonomous_routine", "0");
 
 		OI oi = new OI();
 
 		drive = new RobotDrive(oi.motors[0], oi.motors[1], oi.motors[2],
 				oi.motors[3]);
-
 		stick = new Joystick(oi.controllers[0]);
 		controller = new XboxController(oi.controllers[1]);
 
-		subs = new Subsystem[1];
-		subs[0] = new Drive(drive, controller);
-		subs[1] = new Camera();
+		lift = new Spark(oi.components[0]);
 
-		auto = new AutoSubsystem[1];
-		auto[0] = new Drive(drive, null);
+		subs = new Subsystem[] { new Drive(drive, controller), new Camera(),
+				new Lift(lift) };
+
+		auto = new AutoSubsystem[] { new Drive(drive, null) };
 	}
 
-	@SuppressWarnings("deprecation")
 	@Override
 	public void autonomousInit() {
+		int choose = Integer.parseInt(choice.getTable().getString(
+				"autonomous_routine", "0"));
 		subs[1].start();
 		subs[1].complete(0, null);
-		switch (choice.getTable().getInt("autonomous_routine")) {
+		switch (choose) {
 			case 0:
 				auto[0].start();
 				routine = new Routine() {
@@ -71,15 +74,14 @@ public class Robot extends IterativeRobot {
 		}
 	}
 
-	@SuppressWarnings("deprecation")
 	@Override
 	public void autonomousPeriodic() {
 		for (AutoSubsystem s : auto) {
 			s.tick();
 		}
-		switch (choice.getTable().getInt("autonomous_routine")) {
+		switch (0) {
 			case 0:
-				auto[0].complete(2, routine);
+				auto[0].complete(3, routine);
 				break;
 		}
 	}
@@ -98,16 +100,18 @@ public class Robot extends IterativeRobot {
 	public void teleopPeriodic() {
 		for (Subsystem s : subs) {
 			s.tick();
+			if (!s.getName().equalsIgnoreCase("CameraServer"))
+				s.complete(0, null);
 		}
 	}
 
 	@Override
 	public void disabledInit() {
 		for (Subsystem s : subs) {
-			s.stop();
+			s.kill();
 		}
 		for (AutoSubsystem s : auto) {
-			s.stop();
+			s.kill();
 		}
 	}
 }
