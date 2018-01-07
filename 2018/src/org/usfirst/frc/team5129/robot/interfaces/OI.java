38e0f29924bcc110a26b7824dbd92c127760a5a0
Kyle Guarco <kyleguarco55@gmail.com>
2018-01-07 16:49:30 -0500

    Seperating 2018 code to 'cmd' branch - Delete

--- 2018/src/org/usfirst/frc/team5129/robot/interfaces/OI.java
+++
@@ -1,9 +0,0 @@
-package org.usfirst.frc.team5129.robot.interfaces;
-
-/**
- * This class is the glue that binds the controls on the physical operator
- * interface to the commands and command groups that allow control of the robot.
- */
-public class OI {
-	
-}
